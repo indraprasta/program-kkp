39614ea298dc269f506fb5c76b4200f804fa7369
Robby Awaldi <robbyawaldi@gmail.com>
2019-02-03 18:56:46 +0700

    edit class model nasabah edit file gitignore

--- src/main/java/org/unindra/tabungan/models/Nasabah.java
+++ src/main/java/org/unindra/tabungan/models/Nasabah.java
@@ -1,7 +1,16 @@
 package org.unindra.tabungan.models;
 
 public class Nasabah {
+    private int id_nasabah;
     private String nama_nasabah;
+
+    public int getId_nasabah() {
+        return id_nasabah;
+    }
+
+    public void setId_nasabah(int id_nasabah) {
+        this.id_nasabah = id_nasabah;
+    }
 
     public String getNama_nasabah() {
         return nama_nasabah;
