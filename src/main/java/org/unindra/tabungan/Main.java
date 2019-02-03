c020420b626dbe528ce47468a0999434c998122b
Robby Awaldi <robbyawaldi@gmail.com>
2019-02-03 14:43:40 +0700

    inisialisasi kerangka kerja menggunakan maven

---
+++ src/main/java/org/unindra/tabungan/Main.java
@@ -0,0 +1,16 @@
+package org.unindra.tabungan;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/app.fxml"));
+        primaryStage.setTitle("Tabungan");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/org/unindra/tabungan/controllers/App.java
@@ -0,0 +1,12 @@
+package org.unindra.tabungan.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class App implements Initializable {
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
