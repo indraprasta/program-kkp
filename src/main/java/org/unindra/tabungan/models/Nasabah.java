fe89fcd45af9d40eb0bc3a07687eebc76bf53682
Robby Awaldi <robbyawaldi@gmail.com>
2019-02-03 18:29:29 +0700

    edit model nasabah

--- src/main/java/org/unindra/tabungan/models/Nasabah.java
+++ src/main/java/org/unindra/tabungan/models/Nasabah.java
@@ -2,4 +2,12 @@ package org.unindra.tabungan.models;
 
 public class Nasabah {
     private String nama_nasabah;
+
+    public String getNama_nasabah() {
+        return nama_nasabah;
+    }
+
+    public void setNama_nasabah(String nama_nasabah) {
+        this.nama_nasabah = nama_nasabah;
+    }
 }
